e26626779daf76838d3f9c393dff8225e6a7c7bc
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-02-04 10:49:57 +0530

    add CL X

---
+++ Second Sem/Labs/CL X/Assignment 1/Client.java
@@ -0,0 +1,66 @@
+import java.net.*; 
+import java.io.*; 
+  
+public class Client 
+{ 
+    // initialize socket and input output streams 
+    private Socket socket            = null; 
+    private DataInputStream  input   = null; 
+    private DataOutputStream out     = null; 
+  
+    public Client(String address, int port) 
+    { 
+        // establish a connection 
+        try
+        { 
+            socket = new Socket(address, port); 
+            System.out.println("Connected to Server"); 
+  
+            // takes input from terminal 
+            input  = new DataInputStream(System.in); 
+  
+            // sends output to the socket 
+            out    = new DataOutputStream(socket.getOutputStream()); 
+        } 
+        catch(UnknownHostException u) 
+        { 
+            System.out.println(u); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+   
+        String line = ""; 
+  
+        // END command to shut server down 
+        while (!line.equals("END")) 
+        { 
+            try
+            { 
+                line = input.readLine();
+                out.writeUTF(line); 
+            } 
+            catch(IOException i) 
+            { 
+                System.out.println(i); 
+            } 
+        } 
+        System.out.println("Shutting Down Server");
+        try
+        { 
+            input.close(); 
+            out.close(); 
+            socket.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Client client = new Client("127.0.0.1", 5000); 
+    } 
+} 
---
+++ Second Sem/Labs/CL X/Assignment 1/Server.java
@@ -0,0 +1,60 @@
+import java.net.*; 
+import java.io.*; 
+  
+public class Server 
+{ 
+    //initializing 
+    private Socket          socket   = null; 
+    private ServerSocket    server   = null; 
+    private DataInputStream in       =  null; 
+  
+    public Server(int port) 
+    { 
+        // starts server and waits for a connection 
+        try
+        { 
+            server = new ServerSocket(port); 
+            System.out.println("Server started"); 
+  
+            System.out.println("Waiting for a client to connect"); 
+  
+            socket = server.accept(); 
+            System.out.println("Client connected"); 
+  
+            // takes input from the client socket 
+            in = new DataInputStream( 
+                new BufferedInputStream(socket.getInputStream())); 
+  
+            String line = ""; 
+  
+            // Shuts Server Down on END
+            while (!line.equals("END")) 
+            { 
+                try
+                { 
+                    line = in.readUTF(); 
+                    System.out.println(line); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+            System.out.println("Closing connection"); 
+  
+            // close connection 
+            socket.close(); 
+            in.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        Server server = new Server(5000); 
+    } 
+} 
